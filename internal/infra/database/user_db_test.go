c8405073da65497413bb6633d64ab5a8ca53327b
José C. Martins <zemartins81@gmail.com>
2024-08-09 22:03:06 -0300

    test: Testando userDB

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -13,14 +13,22 @@ func TestCreateUSer(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if err := db.AutoMigrate(&User{}); err != nil {
+	if err := db.AutoMigrate(&entity.User{}); err != nil {
 		t.Error(err)
 	}
 
-	user, _ := entity.NewUser{"Zé", "ze@Ze.com", "123456"}
+	user, _ := entity.NewUser("Zé", "ze@Ze.com", "123456")
 	userDB := NewUser(db)
 
-	err = userDB.Create(&user)
+	err = userDB.Create(user)
 	assert.Nil(t, err)
 
+	var userFound entity.User
+
+	err = db.First(&userFound, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
 }
